fe9aa194ff2d0bcd5d25c21f869f67fb94685a98
Leonardo <leonardotandelicw@outlook.com>
2022-11-22 10:52:28 -0300

    new exercises

--- lista-duas-dimensoes.js
+++ lista-duas-dimensoes.js
@@ -2,4 +2,4 @@ const alunos = ["João", "Juliana", "Ana", "Caio"];
 const medias = [10, 8, 7.5, 9];
 
 const listaDeAlunosEMedias = [alunos, medias];
-console.log(listaDeAlunosEMedias)
+console.log(`A aluna da posição 1 da lista de alunos é ${listaDeAlunosEMedias[0][1]} e a nota dessa aluna é ${listaDeAlunosEMedias[1][1]}.`)
